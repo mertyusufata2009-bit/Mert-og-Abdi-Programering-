5f9149b0a62be2a92b5f9f2716b732c72beaa3c8
MertA25R <mertyusufata2009@gmail.com>
2026-06-22 10:32:09 +0200

    Endelig

--- FunktionerVektor.py
+++ FunktionerVektor.py
@@ -5,8 +5,8 @@ import math
 # Hjælpefunktioner
 # -------------------------
 
-def tjek_nulvektor(laengde):
-    if laengde == 0:
+def tjek_nulvektor(længde):
+    if længde == 0:
         raise ValueError("Nulvektoren kan ikke bruges til denne beregning.")
 
 
@@ -30,12 +30,11 @@ def Vektor2Dskalar(x1, y1, s):
     return x1 * s, y1 * s
 
 
-def Vektor2Dlaengde(x1, y1):
+def Vektor2Dlængde(x1, y1):
     return math.sqrt(x1**2 + y1**2)
 
 
 def Vektor2Dvinkel(x1, y1):
-    
     return math.degrees(math.atan2(y1, x1))
 
 
@@ -44,29 +43,28 @@ def Vektor2Dprikprodukt(x1, y1, x2, y2):
 
 
 def Vektor2Denhedsvektor(x1, y1):
-    laengde = Vektor2Dlaengde(x1, y1)
-    tjek_nulvektor(laengde)
+    længde = Vektor2Dlængde(x1, y1)
+    tjek_nulvektor(længde)
 
-    return x1 / laengde, y1 / laengde
+    return x1 / længde, y1 / længde
 
 
 def VinkelMellem2D(x1, y1, x2, y2):
-    laengde1 = Vektor2Dlaengde(x1, y1)
-    laengde2 = Vektor2Dlaengde(x2, y2)
+    længde1 = Vektor2Dlængde(x1, y1)
+    længde2 = Vektor2Dlængde(x2, y2)
 
-    tjek_nulvektor(laengde1)
-    tjek_nulvektor(laengde2)
+    tjek_nulvektor(længde1)
+    tjek_nulvektor(længde2)
 
-    cos_vinkel = Vektor2Dprikprodukt(x1, y1, x2, y2) / (laengde1 * laengde2)
+    cos_vinkel = Vektor2Dprikprodukt(x1, y1, x2, y2) / (længde1 * længde2)
 
-   
     cos_vinkel = max(-1, min(1, cos_vinkel))
 
     return math.degrees(math.acos(cos_vinkel))
 
 
 def KartesiskTilPolaer(x, y):
-    r = Vektor2Dlaengde(x, y)
+    r = Vektor2Dlængde(x, y)
     vinkel = math.degrees(math.atan2(y, x))
 
     return r, vinkel
@@ -82,24 +80,21 @@ def PolaerTilKartesisk(r, vinkel_grader):
 
 
 def PunktTilVektor2D(x1, y1, x2, y2):
-    # Vektor fra punkt A til punkt B
     return x2 - x1, y2 - y1
 
 
 def Tvaervektor2D(x, y):
-    # En vektor der står vinkelret på den oprindelige
     return -y, x
 
 
 def Projektion2D(x1, y1, x2, y2):
-    # Projektion af vektor A på vektor B
     prik = Vektor2Dprikprodukt(x1, y1, x2, y2)
-    laengde_b_i_anden = x2**2 + y2**2
+    længde_b_i_anden = x2**2 + y2**2
 
-    if laengde_b_i_anden == 0:
+    if længde_b_i_anden == 0:
         raise ValueError("Man kan ikke projicere på nulvektoren.")
 
-    faktor = prik / laengde_b_i_anden
+    faktor = prik / længde_b_i_anden
 
     return faktor * x2, faktor * y2
 
@@ -120,7 +115,7 @@ def Vektor3Dskalar(x1, y1, z1, s):
     return x1 * s, y1 * s, z1 * s
 
 
-def Vektor3Dlaengde(x1, y1, z1):
+def Vektor3Dlængde(x1, y1, z1):
     return math.sqrt(x1**2 + y1**2 + z1**2)
 
 
@@ -129,20 +124,20 @@ def Vektor3Dprikprodukt(x1, y1, z1, x2, y2, z2):
 
 
 def Vektor3Denhedsvektor(x1, y1, z1):
-    laengde = Vektor3Dlaengde(x1, y1, z1)
-    tjek_nulvektor(laengde)
+    længde = Vektor3Dlængde(x1, y1, z1)
+    tjek_nulvektor(længde)
 
-    return x1 / laengde, y1 / laengde, z1 / laengde
+    return x1 / længde, y1 / længde, z1 / længde
 
 
 def VinkelMellem3D(x1, y1, z1, x2, y2, z2):
-    laengde1 = Vektor3Dlaengde(x1, y1, z1)
-    laengde2 = Vektor3Dlaengde(x2, y2, z2)
+    længde1 = Vektor3Dlængde(x1, y1, z1)
+    længde2 = Vektor3Dlængde(x2, y2, z2)
 
-    tjek_nulvektor(laengde1)
-    tjek_nulvektor(laengde2)
+    tjek_nulvektor(længde1)
+    tjek_nulvektor(længde2)
 
-    cos_vinkel = Vektor3Dprikprodukt(x1, y1, z1, x2, y2, z2) / (laengde1 * laengde2)
+    cos_vinkel = Vektor3Dprikprodukt(x1, y1, z1, x2, y2, z2) / (længde1 * længde2)
 
     cos_vinkel = max(-1, min(1, cos_vinkel))
 
@@ -150,7 +145,6 @@ def VinkelMellem3D(x1, y1, z1, x2, y2, z2):
 
 
 def PunktTilVektor3D(x1, y1, z1, x2, y2, z2):
-    # Vektor fra punkt A til punkt B
     return x2 - x1, y2 - y1, z2 - z1
 
 
@@ -163,13 +157,11 @@ def Krydsprodukt(x1, y1, z1, x2, y2, z2):
 
 
 def Tvaervektor3D(x1, y1, z1, x2, y2, z2):
-    # I 3D kan man finde en vektor, der står vinkelret på to vektorer,
-    # ved hjælp af krydsproduktet.
     return Krydsprodukt(x1, y1, z1, x2, y2, z2)
 
 
 def KartesiskTilSfaerisk3D(x, y, z):
-    r = Vektor3Dlaengde(x, y, z)
+    r = Vektor3Dlængde(x, y, z)
     tjek_nulvektor(r)
 
     theta = math.degrees(math.atan2(y, x))
